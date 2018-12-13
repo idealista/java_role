be39c78aefacb62ec3217dc58747864e9292a410
Jose <janoguera@idealista.com>
2018-12-13 13:12:10 +0100

    Add oraclejdk for Dockerhub

--- dockerhub/deploy.sh
+++ dockerhub/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-echo $DOCKER_USERNAME
-
 if [[ -n $1 ]]; then
     TAG_VERSION=$1
 else
@@ -17,10 +15,14 @@ else
     exit 1
 fi
 
+DOCKERHUB_REPO=idealista/jdk:${TAG_VERSION}-${DISTRO}
+
 cd dockerhub
-docker build . -f $DISTRO/Dockerfile -t idealista/java-debian-ansible:${TAG_VERSION}-${DISTRO}
-pipenv run ansible-playbook -i inventory -e "image_tag=${TAG_VERSION}-${DISTRO}" provision_docker_image.yml
+docker build . -f $DISTRO/Dockerfile -t ${DOCKERHUB_REPO}-test
+pipenv run ansible-playbook -i inventory -e "image_tag=${TAG_VERSION}-${DISTRO}-test" provision_docker_image.yml
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+docker commit docker-test-openjdk ${DOCKERHUB_REPO}-openjdk
+docker commit docker-test-oraclejdk ${DOCKERHUB_REPO}-oraclejdk
+docker push
 docker logout
