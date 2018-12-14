ae26397ccd2cb447d98e4498e0b94093468e9adf
Jose <janoguera@idealista.com>
2018-12-14 13:04:20 +0100

    #53 Update README and deploy.sh including Ubuntu 16.04

--- dockerhub/deploy.sh
+++ dockerhub/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+DOCKER_USERNAME="idealista"
+DOCKER_PASSWORD="Pruebas1"
 if [[ -n $1 ]]; then
     TAG_VERSION=$1
 else
@@ -22,7 +24,11 @@ docker build . -f $DISTRO/Dockerfile -t ${DOCKERHUB_REPO}-test
 pipenv run ansible-playbook -i inventory -e "image_tag=${TAG_VERSION}-${DISTRO}-test" provision_docker_image.yml
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
 docker commit docker-test-openjdk ${DOCKERHUB_REPO}-openjdk
+docker push ${DOCKERHUB_REPO}-openjdk
+
 docker commit docker-test-oraclejdk ${DOCKERHUB_REPO}-oraclejdk
-docker push
+docker push ${DOCKERHUB_REPO}-oraclejdk
+
 docker logout
