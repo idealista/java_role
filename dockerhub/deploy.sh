5f1405a67b6ab2c6ae8ea22b2e098c982efcf6d6
dortega <dortega@idealista.com>
2018-12-17 13:31:35 +0100

    Adding deploy sh

---
+++ dockerhub/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+if [[ -n $1 ]]; then
+    TAG_VERSION=$1
+else
+    echo "No tag version specified"
+    exit 1
+fi
+
+if [[ -n $2 ]]; then
+    DISTRO=$2
+else
+    echo "No distribution specified"
+    exit 1
+fi
+
+DOCKERHUB_REPO=idealista/jdk:${TAG_VERSION}-${DISTRO}
+
+cd dockerhub
+docker build . -f $DISTRO/Dockerfile -t ${DOCKERHUB_REPO}-test
+pipenv run ansible-playbook -i inventory -e "image_tag=${TAG_VERSION}-${DISTRO}-test" provision_docker_image.yml
+
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
+docker commit docker-test-openjdk ${DOCKERHUB_REPO}-openjdk
+docker push ${DOCKERHUB_REPO}-openjdk
+
+docker commit docker-test-oraclejdk ${DOCKERHUB_REPO}-oraclejdk
+docker push ${DOCKERHUB_REPO}-oraclejdk
+
+docker logout
